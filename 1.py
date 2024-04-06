b991076a877286548ed8f46e7a491185a52b1dec
rajCodeDev <gamedeveloper284@gmail.com>
2024-04-06 21:48:58 +0530

    raj

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print("raj")
+num1=float(input("Enter the number1:"))
+num2=float(input("Enter the number2:"))
+operator=input("Enter the operator")
+
+
+if  operator=='+':
+    print(num1+num2)
+elif operator=='-':
+     print(num1-num2)
+elif operator=='*':
+     print(num1*num2)
+elif operator=='/':
+     print(num1/num2)
+elif operator=='%':
+     print(num1%num2)
+else:
+     print('invalid')
